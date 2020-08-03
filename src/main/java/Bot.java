d33477517872dbd695cf96866f2694e601581e8d
dimekenn <dimeknn@gmail.com>
2020-08-03 18:50:45 +0600

    dimcommit

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,44 +1,27 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
-
-import java.util.ArrayList;
-import java.util.List;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.LongPollingBot;
 
 public class Bot extends TelegramLongPollingBot {
+
     private static final String BOT_TOKEN = "1249536099:AAGaJt81YbQsHODXXetMkWNn6aO3C1aJxP0";
-    public static final String BOT_NAME = "antikorkz";
+    public static final String BOT_NAME = "antikorkzbot";
 
-    public Bot() {
-    }
 
+    @Override
     public void onUpdateReceived(Update update) {
-        Message message = update.getMessage();
-        if (message != null && message.hasText()) {
-            String var3 = message.getText();
-            byte var4 = -1;
-            switch(var3.hashCode()) {
-                case 1455327635:
-                    if (var3.equals("/start")) {
-                        var4 = 0;
-                    }
-                default:
-                    switch(var4) {
-                        case 0:
-                            this.sendMsg(message, "Чем могу помочь?");
-                    }
+        Message message = new Message();
+        if (message != null && message.hasText()){
+            switch (message.getText()){
+                case "/start":
+                    sendMsg(message, "Чем могу помочь?");
+                    break;
             }
         }
-
     }
 
     private void sendMsg(Message message, String text) {
@@ -47,37 +30,20 @@ public class Bot extends TelegramLongPollingBot {
         sendMessage.setChatId(message.getChatId().toString());
         sendMessage.setReplyToMessageId(message.getMessageId());
         sendMessage.setText(text);
-
         try {
-            this.setButtons(sendMessage);
-            this.execute(sendMessage);
-        } catch (TelegramApiException var5) {
-            var5.printStackTrace();
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
-
     }
 
+    @Override
     public String getBotUsername() {
-        return "antikorkz";
+        return BOT_NAME;
     }
 
+    @Override
     public String getBotToken() {
-        return "1249536099:AAGaJt81YbQsHODXXetMkWNn6aO3C1aJxP0";
-    }
-
-    public void setButtons(SendMessage sendMessage) {
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-        sendMessage.setReplyMarkup(replyKeyboardMarkup);
-        replyKeyboardMarkup.setSelective(true);
-        replyKeyboardMarkup.setResizeKeyboard(true);
-        replyKeyboardMarkup.setOneTimeKeyboard(false);
-        List<KeyboardRow> keyboardRows = new ArrayList();
-        KeyboardRow keyboardFirstRow = new KeyboardRow();
-        KeyboardRow keyboardSecondRow = new KeyboardRow();
-        keyboardFirstRow.add(new KeyboardButton("/start"));
-        keyboardSecondRow.add(new KeyboardButton("/settings"));
-        keyboardRows.add(keyboardFirstRow);
-        keyboardRows.add(keyboardSecondRow);
-        replyKeyboardMarkup.setKeyboard(keyboardRows);
+        return BOT_TOKEN;
     }
 }
