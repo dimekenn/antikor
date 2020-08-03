bd2b91c21d83e08bbd0e943983a4dba8e62df13a
dimekenn <dimeknn@gmail.com>
2020-08-03 11:20:53 +0600

    First commit

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,19 +1,37 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.generics.LongPollingBot;
 
 public class Bot extends TelegramLongPollingBot {
 
+    private static final String BOT_TOKEN = "1249536099:AAGaJt81YbQsHODXXetMkWNn6aO3C1aJxP0";
+    public static final String BOT_NAME = "antikorkzbot";
+
+
+    @Override
     public void onUpdateReceived(Update update) {
-
+        Message message = new Message();
+        if (message != null && message.hasText()){
+            switch (message.getText()){
+                case "/start":
+                    sendMsg(message, "Чем могу помочь?");
+                    break;
+            }
+        }
     }
 
+    private void sendMsg(Message message, String s) {
+    }
+
+    @Override
     public String getBotUsername() {
-        return null;
+        return BOT_NAME;
     }
 
+    @Override
     public String getBotToken() {
-        return null;
+        return BOT_TOKEN;
     }
 }
